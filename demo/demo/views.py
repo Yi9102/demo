9074968cf2298d38314477a79f74f924f41008f4
pyq <673898321@qq.com>
2019-09-08 22:04:21 +0800

    day01 demo

---
+++ demo/demo/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('hello world')
+
+def about(request):
+    return HttpResponse('<h1 style="color:red;">这是一个关于about的文件</h1>')
